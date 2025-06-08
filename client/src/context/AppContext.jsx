b00010453d51ed8964ba1812b5d128e633c8f102
Jayesh Waghmare <waghmarejayesh751@gmail.com>
2025-06-08 21:46:24 +0530

    stripe added

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from 'humanize-duration'
+import { useAuth, useUser } from '@clerk/clerk-react' 
 
 export const AppContext = createContext();
 
@@ -11,6 +12,9 @@ export const AppContextProvider = (props)=>{
     const currency = import.meta.env.VITE_CURRENCY
 
     const navigate = useNavigate();
+
+    const {getToken} = useAuth();
+    const {user} = useUser();
 
     const [allCourses, setAllCourses] = useState([]);
     const [isEducator, setIsEducator] = useState(true);
@@ -70,6 +74,16 @@ export const AppContextProvider = (props)=>{
         fetchAllCourses();
         fetchUserEnrolledCourses();
     }, [])
+
+    const logToken = async () => {
+        console.log(await getToken());
+    }
+
+    useEffect(()=>{
+        if(user){
+            logToken();
+        }
+    },[user]);
 
     const value = {
         currency, allCourses, navigate, calculateRating, isEducator, setIsEducator, calculateNoOfLectures, calculateCourseDuration, calculateChapterTime, enrolledCourses, fetchUserEnrolledCourses
